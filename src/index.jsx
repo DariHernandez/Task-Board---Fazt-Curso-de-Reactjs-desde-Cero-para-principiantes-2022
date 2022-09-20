660e016b9b958552febcca6cf38ef4d445ca8a5b
daridev <hernandezdarifrancisco@gmail.com>
2022-09-20 08:22:56 -0500

    components

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,26 @@
 // Import react modules
-import React from 'react'
-import ReactDom from 'react-dom/client'
+import React from "react";
+import ReactDom from "react-dom/client";
 
 // Get root tag from html
-const root = ReactDom.createRoot (document.querySelector ("#root"))
+const root = ReactDom.createRoot(document.querySelector("#root"));
+
+// Sample component
+function Greeting() {
+  // return html code
+  return (
+    <div> {/* Div container */}
+      <h1>Sample title</h1>
+      <p>Sample text</p>
+    </div>
+  );
+}
 
 // Content inside the root
-root.render (<h1>Hello World</h1>)
+root.render(
+  // Invoke the component "Greeting" many times
+  <div>
+    <Greeting /> {/* self closing tag */}
+    <Greeting></Greeting> {/* regular tag */}
+  </div>
+);
