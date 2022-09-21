bd71168ec140c6058e8d72481b9973db7e2dee29
daridev <hernandezdarifrancisco@gmail.com>
2022-09-21 07:27:17 -0500

    Third party modules

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,10 +1,23 @@
+import './Posts.css'
+
+// import material design icons
+import { MdDownloadForOffline } from 'react-icons/md'
+
 const HandleClick = () => {
-    fetch ("https://jsonplaceholder.typicode.com/posts") // query data to api
-        .then (response => response.json()) // convert data to json
-        .then (data => console.log (data)) // show data in terminal
-        .catch (error => console.error (error)) // if error, catch it and sho in terminal
+    fetch('https://jsonplaceholder.typicode.com/posts') // query data to api
+        .then((response) => response.json()) // convert data to json
+        .then((data) => console.log(data)) // show data in terminal
+        .catch((error) => console.error(error)) // if error, catch it and sho in terminal
 }
 
 export const Posts = () => {
-    return <button onClick={HandleClick}>Request data</button>
+    return (
+        <button onClick={HandleClick}>
+            {/* use icon */}
+            <MdDownloadForOffline />
+            <span>
+                Request data
+            </span>
+        </button>
+    )
 }
