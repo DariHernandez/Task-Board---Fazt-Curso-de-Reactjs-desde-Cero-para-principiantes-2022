624051d5dd154775969fc1a5b54cf79b0269e6d6
daridev <hernandezdarifrancisco@gmail.com>
2022-09-21 08:38:28 -0500

    arrays in react

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,41 @@ import ReactDom from 'react-dom/client'
 
 // Import components
 import { Sample } from './Sample'
-import {Button} from './Button'
-import {TaskCard} from "./Task"
-import {GreatingClass} from './Greeting'
-import {Posts} from './Posts'
+import { Button } from './Button'
+import { TaskCard } from './Task'
+import { GreatingClass } from './Greeting'
+import { Posts } from './Posts'
 
 // Get root tag from html
 const root = ReactDom.createRoot(document.querySelector('#root'))
 
+const users = [
+    {
+        id: 1,
+        name: 'dari dev',
+        image: 'https://robohash.org/dari%20dev',
+    },
+    {
+        id: 2,
+        name: 'user 1',
+        image: 'https://robohash.org/user%201',
+    },
+]
+
 // Content inside the root
 root.render(
     <>
-        <Posts />
+        {/* Loop for each user */}
+        {users.map((user) => {
+            return (
+                // react list element identifier
+                <div key={user.id}>
+                    {/* Use array elements */}
+                    <h1>User name: {user.name}</h1>
+                    <img src={user.image} />
+                    
+                </div>
+            )
+        })}
     </>
 )
